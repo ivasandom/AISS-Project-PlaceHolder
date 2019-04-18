10ece12d477d0f928ff4298a517d47df066f928a
Paco <pacorosa99@gmail.com>
2019-04-18 19:38:40 +0200

    Pequeños cambios en post y put

--- PlaceHolder-AISS/src/main/java/aiss/resources/TrelloResource.java
+++ PlaceHolder-AISS/src/main/java/aiss/resources/TrelloResource.java
@@ -30,7 +30,7 @@ public class TrelloResource {
 		try {
 			cr = new ClientResource(base_url +"/" +name);
 			cr.setEntityBuffering(true);
-			cr.post(datos, Board.class);
+			cr.post(name, Board.class);
 		} catch (ResourceException re) {
 			System.err.println("Error when adding the board: " + name +" " + cr.getResponse().getStatus());
 			success = false;
@@ -45,7 +45,7 @@ public class TrelloResource {
 		try {
 			cr = new ClientResource(base_url +"/" +name);
 			cr.setEntityBuffering(true);
-			cr.post(idBoard, List.class);
+			cr.post(name, List.class);
 		} catch (ResourceException re) {
 			System.err.println("Error when adding the list: " + name +" " + cr.getResponse().getStatus());
 			success = false;
@@ -74,7 +74,7 @@ public class TrelloResource {
 		try {
 			cr = new ClientResource(base_url +"/" +name);
 			cr.setEntityBuffering(true);
-			cr.post(idList, Card.class);
+			cr.post(name, Card.class);
 		} catch (ResourceException re) {
 			System.err.println("Error when adding the card: " + name +" " + cr.getResponse().getStatus());
 			success = false;
@@ -145,7 +145,7 @@ public class TrelloResource {
 		try {
 			cr = new ClientResource(base_url +"/" +id);
 			cr.setEntityBuffering(true);
-			cr.put(id);
+			cr.put(name);
 		} catch (ResourceException re) {
 			System.err.println("Error when updating the playlist: " +name +" " + cr.getResponse().getStatus());
 			success = false;
@@ -160,7 +160,7 @@ public class TrelloResource {
 		try {
 			cr = new ClientResource(base_url +"/" +id);
 			cr.setEntityBuffering(true);
-			cr.put(id);
+			cr.put(name);
 		} catch (ResourceException re) {
 			System.err.println("Error when updating the card: " +name +" " + cr.getResponse().getStatus());
 			success = false;
@@ -175,7 +175,7 @@ public class TrelloResource {
 		try {
 			cr = new ClientResource(base_url +"/" +id);
 			cr.setEntityBuffering(true);
-			cr.put(id);
+			cr.put(name);
 		} catch (ResourceException re) {
 			System.err.println("Error when updating the list: " +name +" " + cr.getResponse().getStatus());
 			success = false;
