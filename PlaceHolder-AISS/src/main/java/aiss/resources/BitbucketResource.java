c3b30c4f26fd73e027eeca606047293670bccdb6
Iván <ivansd.99@gmail.com>
2019-04-19 13:39:59 +0200

    Hechos los resource de Bitbucket y GitLab y hecho web.xml con servlets

--- PlaceHolder-AISS/src/main/java/aiss/resources/BitbucketResource.java
+++ PlaceHolder-AISS/src/main/java/aiss/resources/BitbucketResource.java
@@ -1,5 +1,26 @@
 package aiss.resources;
 
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+import aiss.bitbucket.BitbucketRepository;
+
+
+
 public class BitbucketResource {
 
+	public BitbucketRepository getMyRepos(String username) {
+
+		ClientResource cr = null;
+		BitbucketRepository repositories = null;
+		try {
+			cr = new ClientResource("https://bitbucket.org/2.0/users"+"/"+username+"/repositories");
+			repositories = cr.get(BitbucketRepository.class);
+
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving the repos: " + cr.getResponse().getStatus());
+		}
+		return repositories;
+
+	}
 }
