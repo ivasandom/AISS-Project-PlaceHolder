2edae78745d946c4f78c41af32f5a850ba462be5
nicpazsar <49528314+nicpazsar@users.noreply.github.com>
2019-04-25 16:55:10 +0200

    Add files via upload

---
+++ PlaceHolder-AISS/src/main/java/aiss/model/bitbucket/Author.java
@@ -0,0 +1,57 @@
+
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name",
+    "emailAddress"
+})
+public class Author {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("emailAddress")
+    private String emailAddress;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("emailAddress")
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    @JsonProperty("emailAddress")
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
