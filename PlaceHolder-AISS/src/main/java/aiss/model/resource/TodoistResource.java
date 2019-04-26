247b844b40ac375260116bd8745d55780d145fa1
Jose Gamaza <43765200+diazdevs@users.noreply.github.com>
2019-04-27 00:26:22 +0200

    Update todoist resource

--- PlaceHolder-AISS/src/main/java/aiss/model/resource/TodoistResource.java
+++ PlaceHolder-AISS/src/main/java/aiss/model/resource/TodoistResource.java
@@ -1,12 +1,15 @@
 package aiss.model.resource;
 
-
-
+import org.restlet.data.Form;
 import org.restlet.resource.ClientResource;
 import org.restlet.resource.ResourceException;
 
-import aiss.model.todoist.Event;
+import aiss.model.todoist.Attachment;
+import aiss.model.todoist.Comment;
+import aiss.model.todoist.Label;
 import aiss.model.todoist.Project;
+import aiss.model.todoist.Task;
+import aiss.model.todoist.TaskSimple;
 
 
 public class TodoistResource {
@@ -18,6 +21,9 @@ public class TodoistResource {
         this.accessToken = accessToken;
     }
 	
+	
+	// Project CRUD
+	
 	public Project[] getMyProjects() {
 		
 		ClientResource cr = null;
@@ -25,7 +31,7 @@ public class TodoistResource {
 		String resourceURL = BASE_URL + "/projects?token=" + this.accessToken;
 		
 		try {
-			cr = new ClientResource(resourceURL);
+			cr = new ClientResource(resourceURL);	
 			projects = cr.get(Project[].class);
 		} catch (ResourceException e) {
 			System.err.println("Error when retrieving projects.");
@@ -35,24 +41,8 @@ public class TodoistResource {
 		
 	}
 	
-	public Event[] getActiveTasks(String idProject) {
-		
-		ClientResource cr = null;
-		Event[] events = null;
-		String resourceURL = BASE_URL + "/tasks"+idProject+"?token=" + this.accessToken;
-		
-		try {
-			cr = new ClientResource(resourceURL);
-			events = cr.get(Event[].class);
-		} catch (ResourceException e) {
-			System.err.println("Error when retrieving eventss.");
-		}
-		
-		return events;
-		
-	}
 	public Project getProject(String id) {
-
+		
 		ClientResource cr = null;
 		Project project = null;
 		String resourceURL = BASE_URL + "/projects/" + id + "?token=" + this.accessToken;
@@ -60,138 +50,371 @@ public class TodoistResource {
 		try {
 			cr = new ClientResource(resourceURL);
 			project = cr.get(Project.class);
-
-		} catch (ResourceException re) {
-			System.err.println("Error when retrieving the project: " + cr.getResponse().getStatus());
+		} catch (ResourceException e) {
+			System.err.println("Error when retrieving project");
 		}
-
-		return project;
-
-	}
-	public Event getTask(String id) {
-
-		ClientResource cr = null;
-		Event task = null;
-		String resourceURL = BASE_URL + "/tasks/" + id + "?token=" + this.accessToken;
 		
-		try {
-			cr = new ClientResource(resourceURL);
-			task = cr.get(Event.class);
-		} catch (ResourceException re) {
-			System.err.println("Error when retrieving the board: " + cr.getResponse().getStatus());
-		}
-
-		return task;
-
+		return project;
+		
 	}
 	
-	public boolean addProject(String id) {
-		
-		ClientResource cr = null;
-		boolean success = true;
-		String resourceURL = BASE_URL + "/tasks/" + id + "?token=" + this.accessToken;
-		
-		try {
-			cr = new ClientResource(resourceURL);
-			cr.setEntityBuffering(true);
-			cr.post(" ");
-		} catch (ResourceException re) {
-			System.err.println("Error when adding the task: " + cr.getResponse().getStatus());
-			success = false;
-		}
-		return success;
-	}
-
-
-	public boolean addTask(String name, String idProject) {
-		
-		ClientResource cr = null;
-		boolean success = true;
-		String resourceURL = BASE_URL + "/tasks/" + idProject + "/" + name + "?token=" + this.accessToken;
-		
-		try {
-			cr = new ClientResource(resourceURL);
-			cr.setEntityBuffering(true);
-			cr.post(" ");
-		} catch (ResourceException re) {
-			System.err.println("Error when adding the task: " + name +" " + cr.getResponse().getStatus());
-			success = false;
-		}
-		return success;
-	}
-
 	public boolean deleteProject(String id) {
 		
 		ClientResource cr = null;
-		boolean success = true;
+		boolean deleted = true;
 		String resourceURL = BASE_URL + "/projects/" + id + "?token=" + this.accessToken;
 		
 		try {
 			cr = new ClientResource(resourceURL);
-			cr.setEntityBuffering(true);
 			cr.delete();
-		} catch (ResourceException re) {
-			System.err.println("Error when deleting the project: " + cr.getResponse().getStatus());
-			success = false;
-			throw re;
+		} catch (ResourceException e) {
+			System.err.println("Error when retrieving project");
+			deleted = false;
 		}
 		
-		return success;
+		return deleted;
+		
 	}
-
+	
+	public boolean updateProject(String id, String newName) {
+		
+		ClientResource cr = null;
+		boolean updated = true;
+		String resourceURL = BASE_URL + "/projects/" + id + "?token=" + this.accessToken;
+		
+		try {
+			cr = new ClientResource(resourceURL);
+			Form form = new Form();
+			form.add("name", newName);
+			cr.post(form);
+		} catch (ResourceException re) {
+			System.err.println("Error when updating project");
+			updated = false;
+		}
+		
+		return updated;
+		
+	}
+	
+	public Project createProject(String name) {
+	
+		ClientResource cr = null;
+		Project newProject = null;
+		String resourceURL = BASE_URL + "/projects/?token=" + this.accessToken;
+		
+		try {
+			cr = new ClientResource(resourceURL);
+			Form form = new Form();
+			form.add("name", name);
+			newProject = cr.post(form, Project.class);
+		} catch (ResourceException re) {
+			System.err.println("Error when adding the task: " + cr.getResponse().getStatus());
+		}
+		
+		return newProject;
+	
+	}
+	
+	
+	// Task CRUD
+	
+	public TaskSimple[] getActiveTasks(String idProject) {
+		
+		ClientResource cr = null;
+		TaskSimple[] tasks = null;
+		String resourceURL = BASE_URL + "/tasks?project_id=" + idProject + "&token=" + this.accessToken;
+		
+		try {
+			cr = new ClientResource(resourceURL);
+			tasks = cr.get(TaskSimple[].class);
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving active tasks.");
+		}
+		
+		return tasks;
+		
+	}
+	
+	public Task getTask(String idTask) {
+		
+		ClientResource cr = null;
+		Task task = null;
+		String resourceURL = BASE_URL + "/tasks/" + idTask + "?token=" + this.accessToken;
+		
+		try {
+			cr = new ClientResource(resourceURL);
+			task = cr.get(Task.class);
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving active tasks.");
+		}
+		
+		return task;
+		
+	}
+	
+	public Task createTask(String content, String projectId) {
+		
+		ClientResource cr = null;
+		Task newTask = null;
+		String resourceURL = BASE_URL + "/tasks/?token=" + this.accessToken;
+		
+		try {
+			cr = new ClientResource(resourceURL);
+			Form form = new Form();
+			form.add("content", content);
+			form.add("project_id", projectId);
+			newTask = cr.post(form, Task.class);
+		} catch (ResourceException re) {
+			System.err.println("Error when adding the task: " + cr.getResponse().getStatus());
+		}
+		
+		return newTask;
+	
+	}
+	
+	public boolean updateTask(String idTask, String newContent) {
+		
+		ClientResource cr = null;
+		boolean updated = true;
+		String resourceURL = BASE_URL + "/tasks/" + idTask + "?token=" + this.accessToken;
+		
+		try {
+			cr = new ClientResource(resourceURL);
+			Form form = new Form();
+			form.add("content", newContent);
+			cr.post(form);
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving active tasks.");
+			updated = false;
+		}
+		
+		return updated;
+		
+	}
+	
 	public boolean deleteTask(String id) {
 		
 		ClientResource cr = null;
-		boolean success = true;
+		boolean deleted = true;
 		String resourceURL = BASE_URL + "/tasks/" + id + "?token=" + this.accessToken;
 		
 		try {
 			cr = new ClientResource(resourceURL);
-			cr.setEntityBuffering(true);
+			cr.delete();
+		} catch (ResourceException e) {
+			System.err.println("Error when deleting task");
+			deleted = false;
+		}
+		
+		return deleted;
+	
+	}
+	
+	
+	// CRUD comments
+	
+	public Comment[] getProjectComments(String projectId) {
+		
+		ClientResource cr = null;
+		Comment[] comments = null;
+		String resourceURL = BASE_URL + "/comments?project_id=" + projectId + "&token=" + this.accessToken;
+	
+		try {
+			cr = new ClientResource(resourceURL);
+			comments = cr.get(Comment[].class);
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving project comments.");
+		}
+		
+		return comments;
+	}
+	
+	public Comment[] getTaskComments(String taskId) {
+		
+		ClientResource cr = null;
+		Comment[] comments = null;
+		String resourceURL = BASE_URL + "/comments?task_id=" + taskId + "&token=" + this.accessToken;
+	
+		try {
+			cr = new ClientResource(resourceURL);
+			comments = cr.get(Comment[].class);
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving task comments.");
+		}
+		
+		return comments;
+	}
+	
+	public Comment getComment(String id) {
+		
+		ClientResource cr = null;
+		Comment comment = null;
+		String resourceURL = BASE_URL + "/comments/" + id + "?token=" + this.accessToken;
+	
+		try {
+			cr = new ClientResource(resourceURL);
+			comment = cr.get(Comment.class);
+		} catch (ResourceException re) {
+			System.err.println("Error when retrieving comment.");
+		}
+		
+		return comment;
+		
+	}
+	
+	public Comment createComment(String taskId, String projectId, String content, Attachment attachment) {
+			
+		ClientResource cr = null;
+		Comment comment = null;
+		String resourceURL = BASE_URL + "/comments?token=" + this.accessToken;
+	
+		try {
+			cr = new ClientResource(resourceURL);
+			Form form = new Form();
+			if (taskId != null) {
+				form.add("task_id", taskId);
+			} else {
+				form.add("project_id", projectId);
+			}
+			form.add("content", content);
+			comment = cr.post(form, Comment.class);
+		} catch (ResourceException re) {
+			System.err.println("Error when create comment.");
+		}
+		
+		return comment;
+
+		
+	}
+	
+	public boolean updateComment(String id, String content) {
+		
+		ClientResource cr = null;
+		boolean updated = true;
+		String resourceURL = BASE_URL + "/comments/" + id + "?token=" + this.accessToken;
+	
+		try {
+			cr = new ClientResource(resourceURL);
+			Form form = new Form();
+			form.add("content", content);
+			cr.post(form);
+		} catch (ResourceException re) {
+			System.err.println("Error when create comment.");
+			updated = false;
+		}
+		
+		return updated;
+	}
+	
+	public boolean deleteComment(String id) {
+		
+		ClientResource cr = null;
+		boolean deleted = true;
+		String resourceURL = BASE_URL + "/comments/" + id + "?token=" + this.accessToken;
+	
+		try {
+			cr = new ClientResource(resourceURL);
 			cr.delete();
 		} catch (ResourceException re) {
-			System.err.println("Error when deleting the task: " + cr.getResponse().getStatus());
-			success = false;
-			throw re;
+			System.err.println("Error when create comment.");
+			deleted = false;
 		}
 		
-		return success;
+		return deleted;
 	}
+	
 
-	public boolean updateProject(String id, String name) {
+	// CRUD Labels
+
+	public Label[] getLabels(String taskId) {
 		
 		ClientResource cr = null;
-		boolean success = true;
-		String resourceURL = BASE_URL + "/projects/" + id + "?token=" + this.accessToken;
-		
+		Label[] labels = null;
+		String resourceURL = BASE_URL + "/labels?token=" + this.accessToken;
+	
 		try {
 			cr = new ClientResource(resourceURL);
-			cr.setEntityBuffering(true);
-			cr.put(name);
+			labels = cr.get(Label[].class);
 		} catch (ResourceException re) {
-			System.err.println("Error when updating the project: " +id +" " + cr.getResponse().getStatus());
-			success = false;
+			System.err.println("Error when retrieving labels.");
 		}
-
-		return success;
+		
+		return labels;
 	}
-
-	public boolean updateTask(String name, String idProject) {
+	
+	public Label getLabel(String id) {
 		
 		ClientResource cr = null;
-		boolean success = true;
-		String resourceURL = BASE_URL + "/tasks/" + idProject + "/" + name + "?token=" + this.accessToken;
-		
+		Label label = null;
+		String resourceURL = BASE_URL + "/labels/" + id + "?token=" + this.accessToken;
+	
 		try {
 			cr = new ClientResource(resourceURL);
-			cr.setEntityBuffering(true);
-			cr.put(name);
+			label = cr.get(Label.class);
 		} catch (ResourceException re) {
-			System.err.println("Error when updating the task: " +name +" " + cr.getResponse().getStatus());
-			success = false;
+			System.err.println("Error when retrieving label.");
 		}
+		
+		return label;
+		
+	}
+	
+	public Label createLabel(String name, String order) {
+			
+		ClientResource cr = null;
+		Label label = null;
+		String resourceURL = BASE_URL + "/labels?token=" + this.accessToken;
+	
+		try {
+			cr = new ClientResource(resourceURL);
+			Form form = new Form();
+			form.add("name", name);
+			form.add("order", order);
+			label = cr.post(form, Label.class);
+		} catch (ResourceException re) {
+			System.err.println("Error when creating label.");
+		}
+		
+		return label;
 
-		return success;
+		
+	}
+	
+	public boolean updateLabel(String id, String newName, String newOrder) {
+		
+		ClientResource cr = null;
+		boolean updated = true;
+		String resourceURL = BASE_URL + "/labels/" + id + "?token=" + this.accessToken;
+	
+		try {
+			cr = new ClientResource(resourceURL);
+			Form form = new Form();
+			form.add("name", newName);
+			form.add("order", newOrder);
+			cr.post(form);
+		} catch (ResourceException re) {
+			System.err.println("Error when updating label.");
+			updated = false;
+		}
+		
+		return updated;
+	}
+	
+	public boolean deleteLabel(String id) {
+		
+		ClientResource cr = null;
+		boolean deleted = true;
+		String resourceURL = BASE_URL + "/labels/" + id + "?token=" + this.accessToken;
+	
+		try {
+			cr = new ClientResource(resourceURL);
+			cr.delete();
+		} catch (ResourceException re) {
+			System.err.println("Error when deleting label.");
+			deleted = false;
+		}
+		
+		return deleted;
 	}
 
 
